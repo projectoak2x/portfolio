1424447b7d1004a3e47bd812ef39de89aabf6045
Orlando Sarceda Jr <orlando@jiffiwebhelp.com>
2023-04-26 02:43:10 +0800

    change script to next Script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -60,7 +60,7 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@100;200;300;400;500;600;700;800;900&display=swap'
             rel='stylesheet'
           />
-          <script type='text/javascript' src='bug-min.js'></script>
+          <Script type='text/javascript' src='bug-min.js'></Script>
         </Head>
         <body>
           <Main />
