7e837d9d15eb072bb1c6d7856db9135fc5205edb
Orlando Sarceda Jr <projectoak2x@gmail.com>
2023-06-21 11:44:07 +0800

    remove resume for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,11 +50,11 @@ export default function Home({toggleTheme, theme}:any) {
         </div>
         {/* <Typography>Hello World!</Typography> */}
         <div className='flex gap-x-2'>
-            <a className='group transition duration-300' href='/resume/index.html'>
+            {/* <a className='group transition duration-300' href='/resume/index.html'>
               <div className=''>resume</div>
               
               <span className="block max-w-full group-hover:bg-slate-500 transition-all duration-500 h-0.5 bg-slate-600"></span>
-            </a>
+            </a> */}
            <DarkLightSwitch toggleTheme={toggleTheme} />
         </div>
       </div>
