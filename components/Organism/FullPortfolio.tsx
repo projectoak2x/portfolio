4dd37cad3dadf4378d81e82b4c3915c067d1e5fd
Orlando Sarceda Jr <sarceda.orlandojr@gmail.com>
2024-01-19 03:19:40 +0800

    fix image import

--- components/Organism/FullPortfolio.tsx
+++ components/Organism/FullPortfolio.tsx
@@ -25,13 +25,13 @@ export default function FullPortfolio() {
             <ImageCard imageSrc="/karaoke-app.png" title="Karaoke App" description="A web based karaoke player" skills={["Nextjs", "Firebase", "Youtubeapi"]} />
             <ImageCard imageSrc="/messenger-chatbot.png" title="Messenger Bot" description="Chatgpt integration in messenger" skills={["Openaiapi", "Facebookapi", "Mongodb", "Express"]} />
             <ImageCard imageSrc="/works/ConversionCow.png" title="ConversionCow App" description="Popup plugin for all sites" skills={["Aws", "CI/CD", "Vapor", "Laravel", "Preact"]} />
-            <ImageCard imageSrc="/works/Paraclete.png" title="Paraclete Go" description="Cetralize AI Tools" skills={["Aws", "Azure", "Web Scrapper", "Laravel", "Vue", "Chrome Extension"]} />
+            <ImageCard imageSrc="/works/paraclete.png" title="Paraclete Go" description="Cetralize AI Tools" skills={["Aws", "Azure", "Web Scrapper", "Laravel", "Vue", "Chrome Extension"]} />
             <ImageCard imageSrc="/discord-chatbot.png" title="Discord Bot" description="Chatgpt integration in discord" skills={["Openaiapi", "Discordapi", "Mongodb", "Express"]} />
-            <ImageCard imageSrc="/works/express-time-tracker.png" title="Express Time Tracker" description="Time Tracking App" skills={["Django", "Nginx", "Heroku", "Jquery"]} />
-            <ImageCard imageSrc="/works/wincove.png" title="Cloud Based plant monitoring" description="Uses the IOT to monitor" skills={["Nextjs", "Typescript", "Css", "Javascript"]} />
-            <ImageCard imageSrc="/works/rentall-ph.png" title="RentAll" description="Vehicle Rentall App" skills={["Laravel", "Bootstrap", "Jquery"]} />
-            <ImageCard imageSrc="/works/xauru.png" title="Xauru Site" description="Wordpress Site with custom api" skills={["Wordpress", "Php", "Bootstrap", "Javascript"]} />
-            <ImageCard imageSrc="/works/allaboutai.png" title="AllAboutAI" description="Page for Chatbot Subscription" skills={["Paypal", "Maya", "Markdown"]} />
+            <ImageCard imageSrc="/works/express-time-tracker.PNG" title="Express Time Tracker" description="Time Tracking App" skills={["Django", "Nginx", "Heroku", "Jquery"]} />
+            <ImageCard imageSrc="/works/wincove.png" title="Wincove" description="A news investment site" skills={["Nextjs", "Typescript", "Css", "Javascript"]} />
+            <ImageCard imageSrc="/works/rentall-ph.PNG" title="RentAll" description="Vehicle Rentall App" skills={["Laravel", "Bootstrap", "Jquery"]} />
+            <ImageCard imageSrc="/works/xauru.PNG" title="Xauru Site" description="Wordpress Site with custom api" skills={["Wordpress", "Php", "Bootstrap", "Javascript"]} />
+            <ImageCard imageSrc="/works/allaboutai.PNG" title="AllAboutAI" description="Page for Chatbot Subscription" skills={["Paypal", "Maya", "Markdown"]} />
             <ImageCard imageSrc="/works/skynet.png" title="Skynet Jiffi" description="Ticket Management App" skills={["Laravel", "hubstaff", "React"]} />
             <ImageCard imageSrc="/works/amrap.png" title="Amrap" description="A Music Sharing Platform" skills={["Laravel", "React", "Redis", "aws"]} />
             <ImageCard imageSrc="/works/comfortel.png" title="Comfortel" description="Furniture Browsing Site" skills={["Wordpress", "Vuejs", "Css"]} />
