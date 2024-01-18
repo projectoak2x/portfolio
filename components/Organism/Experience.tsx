16ed4f722a68c70a449f95b0a2bfa40372e4b471
Orlando Sarceda Jr <sarceda.orlandojr@gmail.com>
2024-01-19 02:33:53 +0800

    fix lint

--- components/Organism/Experience.tsx
+++ components/Organism/Experience.tsx
@@ -22,10 +22,10 @@ export default function Experience() {
             Works
           </h1>
           <div className="flex flex-wrap justify-between gap-y-5 mb-5">
-            <ImageCard imageSrc="/karaoke-app.png" title="Karaoke App" description="A web based karaoke player" />
-            <ImageCard imageSrc="/messenger-chatbot.png" title="Messenger Bot" description="Chatgpt integration in messenger" />
-            <ImageCard imageSrc="/discord-chatbot.png" title="Discord Bot" description="Chatgpt integration in discord" />
-            <ImageCard imageSrc="/cloud_base_plant_monitoring.png" title="Cloud Based plant monitoring" description="Uses the IOT to monitor..." />
+          <ImageCard imageSrc="/karaoke-app.png" title="Karaoke App" description="A web based karaoke player" skills={["Nextjs", "Firebase", "Youtubeapi"]} />
+            <ImageCard imageSrc="/messenger-chatbot.png" title="Messenger Bot" description="Chatgpt integration in messenger" skills={["Openaiapi", "Facebookapi", "Mongodb", "Express"]} />
+            <ImageCard imageSrc="/works/ConversionCow.png" title="ConversionCow App" description="Popup plugin for all sites" skills={["Aws", "CI/CD", "Vapor", "Laravel", "Preact"]} />
+            <ImageCard imageSrc="/works/Paraclete.png" title="Paraclete Go" description="Cetralize AI Tools" skills={["Aws", "Azure", "Web Scrapper", "Laravel", "Vue", "Chrome Extension"]} />
           </div>
           <Link href={'/portfolio'} className="bg-slate-700 h-10 leading-10 font-semibold w-24 m-auto" style={{ textAlign: "center", color: "#FFF" }}  >
             View All
